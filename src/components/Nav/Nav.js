29d5894863fc192dec979714c507628d00a8a1c6
jakun <wsparcie.exe@gmail.com>
2022-05-19 13:50:01 +0200

    edit  to pakula

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,10 +6,8 @@ let val;
 window.onscroll = function() {
     if (document.documentElement.scrollTop > 20) {
         nav.classList.add("sticky");
-        scrollBtn.style.display = "block";
     } else {
         nav.classList.remove("sticky");
-        scrollBtn.style.display = "none";
     }
 
 }
