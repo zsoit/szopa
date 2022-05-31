d0b771788d636655f826685e8728621d33db4a7e
jakun <wsparcie.exe@gmail.com>
2022-05-31 14:10:19 +0200

    fix faq

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -13,3 +13,26 @@ function toggleAccordion() {
 }
 
 items.forEach((item) => item.addEventListener('click', toggleAccordion));
+
+
+
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function(item) {
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
+
+try {
+    var hash = window.location.hash + " button";
+    document.querySelector(`${hash}`).setAttribute('aria-expanded', 'true');
+} catch {
+    var ok = 1;
+}
